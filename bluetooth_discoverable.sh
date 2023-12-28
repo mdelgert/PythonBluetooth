3b2bfbcb69b58862d42ec7045a9207a6fca43502
Matthew Elgert <mdelgert@yahoo.com>
2023-12-27 22:57:38 -0500

    add main.conf

--- bluetooth_discoverable.sh
+++ bluetooth_discoverable.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo -e 'power on\nagent on\ndiscoverable on\ndiscoverable-timeout 0' | bluetoothctl
+# echo -e 'power on\nagent on\ndiscoverable on\ndiscoverable-timeout 0' | bluetoothctl
 
 # chmod +x bluetooth_discoverable.sh
 # crontab -e
 # @reboot /home/mdelgert/PythonBluetooth/bluetooth_discoverable.sh
+
+# NOLONGER NEEDED SEE EXAMPLE main.conf
