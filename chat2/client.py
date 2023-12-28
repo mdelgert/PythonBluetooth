f338a7b89e75144f3308d6bc984c08e75ffda5a0
Matthew Elgert <mdelgert@yahoo.com>
2023-12-28 14:22:30 -0500

    refactor

---
+++ chat2/client.py
@@ -0,0 +1,24 @@
+import socket
+
+client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+#client.connect(("<MAC address of the PC you are connecting to>", 4))
+#client.connect(("54:6c:eb:a1:32:44", 4))
+client.connect(("D8:3A:DD:18:15:40", 4))
+
+print(f"Connected!")
+
+try:
+    while True:
+        message = input("Enter message: ")
+        client.send(message.encode('utf-8'))
+        data = client.recv(1024)
+        if not data:
+            break
+        print(f"Received: {data.decode('utf-8')}")
+
+except OSError:
+    pass
+
+print("Disconnected")
+
+client.close()
---
+++ chat2/server.py
@@ -0,0 +1,43 @@
+import socket
+import time
+
+# Steps
+# 1. Device Manager -> Realtek Bluetooth Adapter
+# 2. Right Click -> Properties -> Advanced -> Address
+# 3. Turn on Bluetooth on both devices and make server device visible
+
+server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)  # RFCOMM specific protocol
+#server.bind(("<your BT adapter MAC address>", 4))  # MAC Address and Channel 4
+#server.bind(("54:6c:eb:a1:32:44", 4))  # MAC Address and Channel 4
+server.bind(("D8:3A:DD:18:15:40", 4))  # MAC Address and Channel 4
+
+server.listen(1)
+
+print("Waiting for connection...")
+
+client, addr = server.accept()
+print(f"Accepted connection from {addr}")
+
+try:
+
+    # while True:
+    #     data = client.recv(1024)
+    #     if not data:
+    #         break
+    #     print(f"Received: {data.decode('utf-8')}")
+    #     message = input("Enter message: ")
+    #     client.send(message.encode('utf-8'))
+
+    for i in range(1000):
+            message = "ID: {i}"
+            print(f"{message}")
+            client.send(message.encode('utf-8'))
+            time.sleep(10)
+
+except OSError:
+    pass
+
+print("Disconnected")
+
+client.close()
+server.close()
